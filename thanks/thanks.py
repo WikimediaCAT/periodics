b0c9b5f8191ffda1a8668e026410cd71f0442361
jcreusand <joan.creusandreu@gmail.com>
2019-01-07 20:15:41 +0100

    Afegeixo comentaris a tot el codi

--- thanks/thanks.py
+++ thanks/thanks.py
@@ -3,9 +3,11 @@
 import pywikibot 
 import re
 import sys
+# realment no calen aquests dos imports, eren proves
 from pywikibot.version import getversion
 from pywikibot.login import LoginManager
  
+# aquesta funció tampoc no cal, perquè no s'executa interactivament
 def preguntar():
   while 1:
    sys.stdout.write("Canviar? (s/n/t) ")
@@ -18,7 +20,7 @@ def preguntar():
    if a=='n':
        return -1
 
-    
+# I de fet tampoc no fem cerques, o sigui que també sobra
 def get_search(searchstr):
     params = {
         'action'         :'query',
@@ -32,16 +34,20 @@ def get_search(searchstr):
     sr=pywikibot.data.api.Request(**params).submit()
     return sr
 
+# tres funcions auxiliars, prou evidents
+# dóna el nom en català del mes
 def mescatala(mes):
   nomsmesos = ['gener', 'febrer', u'març', 'abril', 'maig', 'juny', 'juliol', 'agost', 'setembre', 'octubre', 'novembre', 'desembre']
   return nomsmesos[mes-1]
 
+# donada una tupla mes,any, retorna una altra tupla amb el mes anterior
 def prevmes(mes,any):
     if mes > 1:
        return (mes-1,any)
     else:
        return (12,any-1)
 
+# donada una tupla mes,any, retorna una altra tupla amb el mes posterior
 def nextmes(mes,any):
     if mes < 12:
        return (mes+1,any)
@@ -52,6 +58,7 @@ def main():
   #print sys.version
   #print getversion()
 
+  # Llegim paràmetres
   if len(sys.argv) != 3:
       print u"Ús: python thanks.py <any> <mes>".encode("utf-8")
       return
@@ -65,10 +72,12 @@ def main():
      return
 
   messtr= sys.argv[2].strip()
+  # afegim un zero a l'esquerra del mes
   if len(messtr)<2:
       messtr="0"+messtr
   anystr = sys.argv[1].strip()
 
+  # Mirem mesos anterior i posterior, i els passem a string
   (messeg, anyseg) = nextmes(mes,anyllegit)
   messegstr = "%02d" % messeg
   anysegstr = "%d" % anyseg
@@ -77,31 +86,49 @@ def main():
   mesprevstr = "%02d" % mesprev
   anyprevstr = "%d" % anyprev
 
+  # accedim a viquipèdia per llegir el registre d'agraïments
+  # construïm end i start a partir de les variables anteriors
   casite = pywikibot.Site('ca')
   registre = casite.logevents(logtype="thanks",
                               end   =  anystr+"-"+messtr+"-01T00:00:00",
                               start =  anysegstr+"-"+messegstr+"-01T00:00:00",
                               #total=10,
                               )
+  # creem els conjunts de gent que rep agraïments, i el que els fa
+  # de moment, buits
   agraciats={}
   graciadors={} 
 
-  if registre != None:
-     for l in registre:
+  if registre != None:      # si hem llegit alguna cosa del registre
+     for l in registre:     # l'anem llegint registre per registre
+       # el paràmetre page() ens diu qui ha rebut l'agraïment, per tant
+       # en sumem 1.
+       # la primera vegada que aparegui un usuari determinat, donarà error
+       # llavors (dins l'except), aprofitem per inicialitzar-lo a 1
        try:
          agraciats[l.page()]=agraciats[l.page()]+1
        except KeyError:
          agraciats[l.page()]=1
+       # el paràmetre user() ens diu qui ha fet l'agraïment, per tant
+       # fem el mateix procés que abans, però per als que agraeixen
        try:
          graciadors[l.user()]=graciadors[l.user()]+1
        except KeyError:
          graciadors[l.user()]=1
 
+  # Ara ja tenim les dades, només cal crear la pàgina corresponent i
+  # formatejar-la amb una mica de gràcia.
+  # Primer, la navegació a mes anterior i mes següent
   txtout = u"<center>[[Usuari:JoRobot/Agraïments/"+anyprevstr+"/"+mesprevstr+u"|Mes anterior]] {{·}} '''Historial''' {{·}} [[Usuari:JoRobot/Agraïments/"+anysegstr+"/"+messegstr+u"|Mes següent]]</center>\n"
+  # Ara centrem les taules
   txtout = txtout + u"<div align=\"center\">\n{{Columnes|66%}}\n"
+  # Posem capçaleres de les taules
   txtout = txtout + u"{| class=\"wikitable sortable\"\n|+ Agraïments rebuts per usuari durant el mes de "+mescatala(mes)+" de "+anystr+"\n"
   txtout = txtout+u"|-\n! scope=\"col\" | Usuari\n! scope=\"col\" | Agraïments\n"
 
+  # Aquest bucle és per cada fila. Primer els que reben agraïments
+  # No ho posem en cap ordre especial, tal com raja del conjunt que hem anat
+  # omplint. Com que fem la taula sortable, ja l'ordenarà el lector si vol
   for u, num in agraciats.items():
       #print dir(u)
       txtout = txtout+"|-\n"
@@ -109,21 +136,33 @@ def main():
       usuaristr = u"[[Usuari:"+us+"|"+us+"]]"
       txtout = txtout+"| "+ usuaristr +"||"+ ("%d" % num)+"\n"
 
+  # Tanquem la taula, canvi de columna
   txtout = txtout+"|}\n"
   txtout = txtout+"{{Columna nova}}\n"
 
+  # ara ve la capçalera de la segona taula, la dels que fan agraïments
   txtout = txtout + u"{| class=\"wikitable sortable\"\n|+ Agraïments fets per usuari durant el mes de "+mescatala(mes)+" de "+anystr+"\n"
   txtout = txtout+u"|-\n! scope=\"col\" | Usuari\n! scope=\"col\" | Agraïments\n"
+  # bucle amb les files dels que agraeixen
   for u, num in graciadors.items():
       txtout = txtout+"|-\n"
       usuaristr = u"[[Usuari:"+u+"|"+u+"]]"
       txtout = txtout+"| "+ usuaristr +"||"+ ("%d" % num)+"\n"
+  # Tanquem taula, columnes i secció
   txtout = txtout+"|}\n"
   txtout = txtout+"{{Final columnes}}\n</div>\n"
+  # Categoria, que no falti
   txtout = txtout+u"[[Categoria:Registre d'agraïments per mesos]]\n"
 
+  # I ara gravem la pàgina. Primer generem el nom
   titolpag = u"Usuari:JoRobot/Agraïments/"+anystr+"/"+messtr
+  # I amb això ho gravem
   pag = pywikibot.Page(casite,titolpag)
   pag.put(txtout,comment=u"Estadístiques d'agraïments",force=True)
 
-main()
+# Ara cridem el main que hem definit, a la pythonesca manera
+if __name__ == '__main__':
+    try:
+        main()
+    finally:
+        pass
--- thanks/thankslaunch.sh
+++ thanks/thankslaunch.sh
@@ -7,12 +7,21 @@ PATH=/bin:/usr/bin
 # Això des de toolforge
 HOMEBOT=/data/project/jorobot/periodics/thanks
 
+# Aquest shell script està pensat per passar-lo en qualsevol moment del mes
+# i possiblement unes quantes vegades. Només té efecte la primera vegada que
+# es passa. Les subsegüents, no fa res.
+
+# Llegim el fitxer que diu l'últim mes per al qual s'han creat estadístiques
+# el format és aaaamm
 ultimany=`cat $HOMEBOT/thanksbot.dat | cut -c1-4`
 ultimmes=`cat $HOMEBOT/thanksbot.dat | cut -c5-7`
 
+# Mirem en quin any i mes som
 anyactual=`date +"%Y"`
 mesactual=`date +"%m"`
 
+# mirem quin tocaria fer segons el que hem llegit a thanksbot.dat
+# tenim en compte que després del desembre canvia l'any
 if [ X$ultimmes = "X12" ]
 then noumes=01
      nouany=`echo "$ultimany" | awk '{out=$0+1;printf out}'`
@@ -20,11 +29,14 @@ else noumes=`echo "$ultimmes" | awk '{out=$0+1;printf "%02d",out}'`
      nouany=$ultimany
 fi
 
+# aquí mirem si el mes que estem ja està fet. En aquest cas, pleguem
 if [ X$anyactual = "X$nouany" ]
 then if [ X$mesactual = "X$noumes" ]
 then return		# ja l'hem fet, no hi tornem
 fi
 fi
 
+# si arribem aquí, és que s'ha d'executar
+# thanks.py agafa com a paràmetres l'any i mes del que vols fer estadístiques
 python $HOMEBOT/thanks.py $nouany $noumes
 echo $nouany$noumes > $HOMEBOT/thanksbot.dat  # posem la data de l'últim mes fet
