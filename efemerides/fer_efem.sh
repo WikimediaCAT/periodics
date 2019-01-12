5228a8a3dd83ebe45df376fcb659fe253d6390c7
jcreusand <joan.creusandreu@gmail.com>
2019-01-12 17:20:50 +0100

    Comentari per executar fora de toolforge

--- efemerides/fer_efem.sh
+++ efemerides/fer_efem.sh
@@ -6,6 +6,7 @@ PATH=/bin:/usr/bin
 #0	15	*	*	3 /usr/bin/jsub -N fer_efem -once -quiet /data/project/jorobot/periodics/efemerides/fer_efem.sh
 # O sigui, el dia 3 de la setmana (dimecres) a les 3 de la tarda
 HOMEBOT=/data/project/jorobot/periodics/efemerides
+#HOMEBOT=~/programes/periodics/efemerides
 # Aquesta variable té els dies d'antelació amb què creem les plantilles
 ANTELACIO=14
 
