fb214a628b182ce06248d566b2c09fd5bc4ed077
jcreusand <joan.creusandreu@gmail.com>
2019-03-09 22:45:26 +0100

    Canvis en els comentaris

--- mesllegits/mesllegits.sh
+++ mesllegits/mesllegits.sh
@@ -3,8 +3,9 @@ PATH=/bin:/usr/bin
 
 # Des de toolforge
 # S'engega per crontab amb una línia com aquesta (però sense el comentari)
-#10	3-7	*	*	* /usr/bin/jsub -N mesllegits -once -quiet /data/project/jorobot/periodics/mesllegits/mesllegits.sh
-# O sigui, cada dia, a l'hora i 10, de 3 a 7 del matí
+#10	1-6	*	*	* /usr/bin/jsub -N mesllegits -once -quiet /data/project/jorobot/periodics/mesllegits/mesllegits.sh
+# O sigui, cada dia, a l'hora i 10, de 1 a 6 del matí (en realitat, de 2 a 7
+# que el toolserver va a hora UTC)
 HOMEBOT=/data/project/jorobot/periodics/mesllegits
 #HOMEBOT=~/programes/periodics/mesllegits
 
