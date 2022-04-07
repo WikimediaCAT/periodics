cfada7f0ff1eacce14cebcedce5f5aa31c8f5edb
jcreusand <joan.creusandreu@gmail.com>
2022-04-07 20:19:20 +0200

    Ara l'scheduling és diferent

--- efemerides/genbd.sh
+++ efemerides/genbd.sh
@@ -5,12 +5,12 @@ PATHVENV=/data/project/jorobot/periodics/efemerides/pyvenv_pel_batch
 
 # Des de toolforge
 # S'engega per crontab amb una línia com aquesta (però sense el comentari)
-#0	20	14	*	* /usr/bin/jsub -N gensub -once -quiet /data/project/jorobot/periodics/efemerides/gensub.sh
-# O sigui, el dia 14 de cada mes, a les 8 del vespre
+#0	20	7	*	* /usr/bin/jsub -N gensub -once -quiet /data/project/jorobot/periodics/efemerides/gensub.sh
+# O sigui, el dia 7 de cada mes, a les 8 del vespre, hora de toolforge
 # Tots els mesos, i qualsevol dia de la setmana.
 
 # Un cop migrat a Kubernetes, s'engega un cop amb
-# toolforge-jobs run fer-efem --command "periodics/efemerides/gensub.sh" --image tf-python39 --schedule "0 20 14 * *"
+# toolforge-jobs run fer-efem --command "periodics/efemerides/gensub.sh" --image tf-python39 --schedule "0 20 7 * *"
 
 HOMEBOT=~/periodics/efemerides
 
