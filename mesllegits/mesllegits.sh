c9c8a49a9a1dba75b2c13da50e358a121b2bbe7a
jcreusand <joan.creusandreu@gmail.com>
2022-04-08 00:20:04 +0200

    Migrem a kubernetes

--- mesllegits/mesllegits.sh
+++ mesllegits/mesllegits.sh
@@ -6,11 +6,14 @@ PATH=/bin:/usr/bin
 #10	1-6	*	*	* /usr/bin/jsub -N mesllegits -once -quiet /data/project/jorobot/periodics/mesllegits/mesllegits.sh
 # O sigui, cada dia, a l'hora i 10, de 1 a 6 del matí (en realitat, de 2 a 7
 # que el toolserver va a hora UTC)
-HOMEBOT=/data/project/jorobot/periodics/mesllegits
-#HOMEBOT=~/programes/periodics/mesllegits
 
-PYTHONPATH=/data/project/shared/pywikipedia/core
+# Amb Kubernetes, s'engega un cop amb
+# toolforge-jobs run mesllegits --command "periodics/mesllegits/mesllegits.sh" --image tf-python39 --schedule "10 1-6 * * *"
+
+HOMEBOT=~/periodics/mesllegits
+#HOMEBOT=~/programes/periodics/mesllegits
+PYVENV=~/periodics/efemerides/pyvenv_pel_batch
 
 cd $HOMEBOT
 
-python3 $HOMEBOT/mesllegits.py
+$PYVENV/bin/python3 $HOMEBOT/mesllegits.py
