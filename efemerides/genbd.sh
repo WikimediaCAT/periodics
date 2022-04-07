e3fdf1b2f36e01ae677b5222485713d08d85a9bf
jcreusand <joan.creusandreu@gmail.com>
2022-04-07 20:16:07 +0200

    Adaptem genbd a entorn kubernetes

--- efemerides/genbd.sh
+++ efemerides/genbd.sh
@@ -1,12 +1,18 @@
 #!/bin/sh
 PATH=/bin:/usr/bin
 
+PATHVENV=/data/project/jorobot/periodics/efemerides/pyvenv_pel_batch
+
 # Des de toolforge
 # S'engega per crontab amb una línia com aquesta (però sense el comentari)
 #0	20	14	*	* /usr/bin/jsub -N gensub -once -quiet /data/project/jorobot/periodics/efemerides/gensub.sh
 # O sigui, el dia 14 de cada mes, a les 8 del vespre
 # Tots els mesos, i qualsevol dia de la setmana.
-HOMEBOT=/data/project/jorobot/periodics/efemerides
+
+# Un cop migrat a Kubernetes, s'engega un cop amb
+# toolforge-jobs run fer-efem --command "periodics/efemerides/gensub.sh" --image tf-python39 --schedule "0 20 14 * *"
+
+HOMEBOT=~/periodics/efemerides
 
 # si no existeix el directori, el creem, però això només hauria de ser la
 # primera vegada de totes
@@ -27,6 +33,6 @@ fi
 # per exemple: ./gensub.sh 2
 mesqueve=`date +'%m' -d "+$antelacio month"`
 
-python3 ./efemerides.py $mesqueve
+./pyvenv_pel_batch/bin/python3 ./efemerides.py $mesqueve
 
 ./comprovarbd.sh $mesqueve
