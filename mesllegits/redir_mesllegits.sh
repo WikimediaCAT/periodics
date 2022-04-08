3071eca3790761791f72fe056298b1c985bfedeb
jcreusand <joan.creusandreu@gmail.com>
2022-04-08 23:23:35 +0200

    Modificacions per adaptar-ho a Kubernetes

--- mesllegits/redir_mesllegits.sh
+++ mesllegits/redir_mesllegits.sh
@@ -7,11 +7,13 @@ PATH=/bin:/usr/bin
 # O sigui, cada dia, a les 00:10, (en realitat, 01:10, que el toolserver va
 # a hora UTC)
 
-HOMEBOT=/data/project/jorobot/periodics/mesllegits
-#HOMEBOT=~/programes/periodics/mesllegits
+# Amb Kubernetes, s'engega un cop amb
+# toolforge-jobs run redir --command "periodics/mesllegits/redir_mesllegits.sh" --image tf-python39 --schedule "10 1-6 * * *"
 
-PYTHONPATH=/data/project/shared/pywikipedia/core
+HOMEBOT=~/periodics/mesllegits
+#HOMEBOT=~/programes/periodics/mesllegits
+PYVENV=~/periodics/efemerides/pyvenv_pel_batch
 
 cd $HOMEBOT
 
-python3 $HOMEBOT/redir_mesllegits.py
+$PYVENV/bin/python3 $HOMEBOT/redir_mesllegits.py
